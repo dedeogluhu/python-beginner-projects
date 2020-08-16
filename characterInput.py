5e97eca826e29ce7258ad5fc17ccf724076502aa
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-08-16 13:31:39 +0300

    Update characterInput.py

--- characterInput.py
+++ characterInput.py
@@ -3,7 +3,7 @@ age = int(input("Please enter your age:"))
 
 if type(name) == str:
     if type(age) == int:
-        print( name,"You are ",age, "years old and","you will be 100 after", (100 - age), "years")
+        print(f"{name} you are {age} years old and you will be 100 after {100 - age} years")
     else:
         print("Please enter a valid number")
 else:
